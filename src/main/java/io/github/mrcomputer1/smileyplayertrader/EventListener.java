4faecbb8bc3a626186021d6b3c2baed126a0fb08
Mrcomputer1 <mrcomputer1email@gmail.com>
2019-12-31 23:23:34 +1000

    Forgot an !

--- src/main/java/io/github/mrcomputer1/smileyplayertrader/EventListener.java
+++ src/main/java/io/github/mrcomputer1/smileyplayertrader/EventListener.java
@@ -52,7 +52,7 @@ public class EventListener implements Listener {
                         e.getWhoClicked().sendMessage(ChatColor.GREEN + "You purchased an item from " + store.getName());
                     }
                     store.sendMessage(ChatColor.GREEN + e.getWhoClicked().getName() + " just purchased " + mi.getSelectedRecipe().getResult().getType() + "!");
-                    if(ItemUtil.doesPlayerHaveItem(store, mi.getSelectedRecipe().getResult())){
+                    if(!ItemUtil.doesPlayerHaveItem(store, mi.getSelectedRecipe().getResult())){
                         store.sendMessage(ChatColor.RED + mi.getSelectedRecipe().getResult().getType().toString() + " is now out of stock!");
                     }
                 }else{
