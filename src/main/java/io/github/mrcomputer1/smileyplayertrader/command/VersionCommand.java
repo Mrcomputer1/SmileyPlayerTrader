6e5dfcbcdd4bd4eb9c782c4c7502e28548fd2152
Mrcomputer1 <mrcomputer1email@gmail.com>
2024-07-19 21:19:50 +1000

    Add option for translator to get credit message in `/spt version`.

--- src/main/java/io/github/mrcomputer1/smileyplayertrader/command/VersionCommand.java
+++ src/main/java/io/github/mrcomputer1/smileyplayertrader/command/VersionCommand.java
@@ -2,6 +2,7 @@ package io.github.mrcomputer1.smileyplayertrader.command;
 
 import io.github.mrcomputer1.smileyplayertrader.SmileyPlayerTrader;
 import io.github.mrcomputer1.smileyplayertrader.util.I18N;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class VersionCommand implements ICommand{
@@ -9,5 +10,9 @@ public class VersionCommand implements ICommand{
     public void onCommand(CommandSender sender, String[] args) {
         sender.sendMessage(I18N.translate("&bSmiley Player Trader by Mrcomputer1 and Semisol. Version %0%.",
                 SmileyPlayerTrader.getInstance().getDescription().getVersion()));
+
+        String credit = I18N.translate("$$credit$$");
+        if(!credit.isEmpty())
+            sender.sendMessage(ChatColor.AQUA + credit);
     }
 }
